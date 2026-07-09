4c6a5b21e33680120ed1768986aacfebf63b0ca5
vanessa2103206 <vanessac2016@libero.it>
2026-07-09 10:49:31 +0200

    Aggiunto file main.js separato

---
+++ main.js
@@ -0,0 +1,14 @@
+let num1 = 15;
+let num2 = 42;
+
+console.log("Il valore della variabile num1 è: " + num1);
+console.log("Il valore della variabile num2 è: " + num2);
+
+let stringa = "Benvenuta nel corso!";
+console.log("Valore iniziale della stringa: " + stringa);
+
+stringa = "Questo è il nuovo valore modificato";
+console.log("Nuovo valore della stringa: " + stringa);
+
+const PIGRECO = 3.14159265359;
+console.log("Il valore di PIGRECO è: " + PIGRECO);
